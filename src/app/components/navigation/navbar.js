5d8b0bd26588b01a2e3f7690b4453f917ef6d4ba
anhnlh <aln4739@rit.edu>
2024-02-24 23:05:12 -0500

    added responsive hamburger

--- src/app/components/navigation/navbar.js
+++ src/app/components/navigation/navbar.js
@@ -1,36 +1,44 @@
-import Image from "next/image";
+"use client";
 import Link from "next/link";
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import React, { useState } from "react";
 
-const Navbar = () => {
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="flex py-8 text-2xl">
-      <Link href="/" className="flex-none font-black ml-11">
+    <nav className="flex items-center h-24 py-8 text-2xl bg-slate-950">
+      <Link href="/" className="ml-6 font-black ">
         anhnlh
       </Link>
-      <div className="grow mr-11">
-        <ul className="justify-center hidden lg:flex">
-          <li className="mx-6">
-            <Link href="/" className="p-2 text-white hover:text-gray-300">
-              Home
-            </Link>
-          </li>
-          <li className="mx-6">
-            <Link
-              href="/#projects"
-              className="p-2 text-white hover:text-gray-300"
-            >
-              Projects
-            </Link>
-          </li>
-          <li className="mx-6">
-            <Link href="/cv" className="p-2 text-white hover:text-gray-300">
-              Curriculum Vitae
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <ul className="justify-center hidden font-medium grow mr-11 md:flex">
+        <li className="mx-3">
+          <Link href="/" className="p-2 text-white hover:text-gray-300">
+            Home
+          </Link>
+        </li>
+        <li className="mx-3">
+          <Link
+            href="/#projects"
+            className="p-2 text-white hover:text-gray-300"
+          >
+            Projects
+          </Link>
+        </li>
+        <li className="mx-3">
+          <Link href="/cv" className="p-2 text-white hover:text-gray-300">
+            Curriculum Vitae
+          </Link>
+        </li>
+      </ul>
+      <Bars3Icon
+        onClick={handleClick}
+        className="ml-auto mr-6 w-14 h-14 md:hidden"
+      />
     </nav>
   );
-};
-
-export default Navbar;
+}
