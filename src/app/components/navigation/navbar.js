b846cb216586e75391661834645974bbe2ef4c47
anhnlh <aln4739@rit.edu>
2024-02-25 12:30:21 -0500

    added some colors, cool 3d effect on hero section

--- src/app/components/navigation/navbar.js
+++ src/app/components/navigation/navbar.js
@@ -11,34 +11,36 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="flex items-center h-24 py-8 text-2xl bg-slate-950">
+    <nav className="sticky top-0 z-10 flex items-center w-full h-24 py-8 text-2xl bg-slate-800/90">
       <Link href="/" className="ml-6 font-black ">
         anhnlh
       </Link>
       <ul className="justify-center hidden font-medium grow mr-11 md:flex">
         <li className="mx-3">
-          <Link href="/" className="p-2 text-white hover:text-gray-300">
+          <Link href="/" className="p-2 text-white hover:text-indigo-400">
             Home
           </Link>
         </li>
         <li className="mx-3">
           <Link
             href="/#projects"
-            className="p-2 text-white hover:text-gray-300"
+            className="p-2 text-white hover:text-indigo-400"
           >
             Projects
           </Link>
         </li>
         <li className="mx-3">
-          <Link href="/cv" className="p-2 text-white hover:text-gray-300">
+          <Link href="/cv" className="p-2 text-white hover:text-indigo-400">
             Curriculum Vitae
           </Link>
         </li>
       </ul>
-      <Bars3Icon
+      <button
         onClick={handleClick}
         className="ml-auto mr-6 w-14 h-14 md:hidden"
-      />
+      >
+        <Bars3Icon />
+      </button>
     </nav>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,21 @@
+import { CardContainer } from "./components/ui/3d-card";
+
 export default function Home() {
   return (
-    <main>
+    <main className="theme-zinc">
       {/* Hero section: bio on the left, name and subtitle on the right */}
-      <section className="flex flex-col justify-center min-h-screen px-5 bg-white">
+      <section className="flex flex-col justify-center min-h-screen px-10 pb-10 sm:px-20 md:px-24 bg-gradient-to-b from-indigo-600 to-cyan-300">
         {/* Name, subtitle */}
-        <div className="flex flex-col">
-          <h1 className="font-bold text-gray-800 text-8xl md:text-9xl">
-            Anh <br /> Nguyen
+        <CardContainer className="flex flex-col w-full py-12 shadow-xl sm:py-16 md:py-24 rounded-2xl bg-gradient-to-b from-sky-400 to-purple-500 hover:shadow-2xl">
+          <h1 className="text-5xl font-bold text-center text-white sm:text-6xl md:text-9xl">
+            anhnlh
           </h1>
-          <p className="mt-8 text-3xl text-gray-600 md:text-4xl">
+          <p className="mt-4 text-xl font-medium text-center text-white sm:text-3xl sm:mt-6 md:text-4xl md:mt-11">
             Software Engineer
           </p>
-        </div>
+        </CardContainer>
         {/* Bio */}
-        <p className="text-xl text-gray-600 mt-11 md:text-2xl">
+        <p className="text-xl text-justify text-slate-50 md:text-2xl">
           Senior BS Computer Science student at the Rochester Institute of
           Technology, currently working at Amplify Education and building
           educational software for K-12 students.
