25900cf80351b9a7d14882225018dd01c59157ea
anhnlh <anh.lh.nguyen42@gmail.com>
2025-04-04 17:13:40 -0400

    sticky navbar, projects page styling (content todo), basic text page for individual projects, page icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        projects: "projects/index.html",
+        portfolio: "projects/portfolio.html",
+      },
+    },
+  },
   plugins: [tailwindcss()],
 });
