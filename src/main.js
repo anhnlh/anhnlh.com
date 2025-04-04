c8b52fa9d8c9f4643509d0529d8c9aee6a098eee
anhnlh <anh.lh.nguyen42@gmail.com>
2025-04-04 10:16:10 -0400

    updated content and restyled sections, made a timeline for experiences

---
+++ src/main.js
@@ -0,0 +1,3 @@
+const menu = document.getElementById("menu");
+const menuButton = document.getElementById("menu-toggle");
+menuButton.addEventListener("click", () => menu.classList.toggle("hidden"));
